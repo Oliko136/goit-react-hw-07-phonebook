c2a223e3d03c08a117d2eec9842a4af57c28eb4b
Oliko136 <olchick136@gmail.com>
2024-02-24 18:47:12 +0400

    Selectors refactored

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,15 +1,13 @@
+import { createSelector } from "@reduxjs/toolkit";
+
+export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 export const selectFilter = state => state.filter;
-export const selectFilteredContacts = ({contacts, filter}) => {
-    if (!filter) {
-        return contacts.items;
+export const selectFilteredContacts = createSelector(
+    [selectContacts, selectFilter],
+    (contacts, filter) => {
+        return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
     }
-    const filteredContacts = contacts.items.filter(({ name }) => {
-        const normalizedName = name.toLowerCase();
-        return (
-            normalizedName.includes(filter)
-        )
-    })
-    return filteredContacts;
-}
+)
+    
